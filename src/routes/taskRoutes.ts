ae2cc19c8c3b4f7cd6d5d3fe7afc27fdac63a729
Hselit <tileshkathiresan@gmail.com>
2026-06-27 14:48:14 +0530

    updated-route-file

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
-
 import { validate } from "../middleware/validator";
+import { createTask, getAllTasks, deleteTask, updateTask,getTaskById } from "../controller/taskController";
 
 const router = Router();
 
-// router.get("/", );
+router.get("/", getAllTasks);
 
-// router.get("/:id",validate(getBookSchema),getBookDetails);
+router.get("/:id", validate(), getTaskById);
 
-// router.post("/",validate(createBookSchema),addBook);
+router.post("/", validate(), createTask);
 
-// router.delete("/:id", validate(getBookSchema),removeBook);
+router.put("/:id", validate(), updateTask);
+
+router.delete("/:id", validate(), deleteTask);
 
 export default router;
